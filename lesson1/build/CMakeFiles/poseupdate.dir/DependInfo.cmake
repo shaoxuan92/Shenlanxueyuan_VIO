cc6fcf98ebb07ca9a662002c1758f309c498f09a
XaoSHuan <shaoxuan92@163.com>
2019-06-16 20:50:40 +0800

    lesson 1

--- lesson1/build/CMakeFiles/poseupdate.dir/DependInfo.cmake
+++ lesson1/build/CMakeFiles/poseupdate.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/lin/Documents/XaoSHuan/课件与作业/lesson_1.cpp" "/home/lin/Documents/XaoSHuan/课件与作业/build/CMakeFiles/poseupdate.dir/lesson_1.cpp.o"
+  "/home/lin/Documents/XaoSHuan/VIOStudy/lesson1/lesson_1.cpp" "/home/lin/Documents/XaoSHuan/VIOStudy/lesson1/build/CMakeFiles/poseupdate.dir/lesson_1.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- lesson1/build/cmake_install.cmake
+++ lesson1/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/lin/Documents/XaoSHuan/课件与作业
+# Install script for directory: /home/lin/Documents/XaoSHuan/VIOStudy/lesson1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/lin/Documents/XaoSHuan/课件与作业/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/lin/Documents/XaoSHuan/VIOStudy/lesson1/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
